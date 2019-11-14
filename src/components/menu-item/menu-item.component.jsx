0d603ffe26e9b8ced04e42a23de67c6d3415218e
Poli <stone.horse@hotmail.com>
2019-11-14 13:44:49 +0100

    Added links pages to the web

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import './menu-item.styles.scss';
+import { withRouter } from "react-router-dom";
+import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div className='background-image' style={{
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{
         backgroundImage: `url(${imageUrl})`
-    }}/>
+      }}
+    />
     <div className="content">
       <div className="title">{title.toUpperCase()}</div>
       <span className="subtitle">SHOP NOW</span>
@@ -13,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
