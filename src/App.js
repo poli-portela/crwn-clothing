fc27b2a4458bd8136ebbb15ad9a2fdd1659f6915
Poli <stone.horse@hotmail.com>
2019-11-15 13:05:05 +0100

    Preview collection with item collection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from './pages/shop/shop.component';
 import "./App.css";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 class App extends React.Component {
   render() {
@@ -16,7 +11,7 @@ class App extends React.Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/hats" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
