dd9b7c30dfbff73b0a78dfcf7f3d8573592d5f8f
Poli <stone.horse@hotmail.com>
2020-01-16 13:58:12 +0100

    Cheges in several parts of the application an data structure

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-import SHOP_DATA from "./shop.data.js";
-import CollectionPreview from "../../components/preview-collection/preview-collection.component";
+import { Route } from 'react-router-dom'; 
+import CollectionPage from '../collections/collection.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionsProps }) => (
-          <CollectionPreview key={id} {...otherCollectionsProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
+  
 export default ShopPage;
--- src/redux/roor-reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import userRedeucer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer';
-
-export default combineReducers({
-    user: userRedeucer,
-    cart: cartReducer
-})
